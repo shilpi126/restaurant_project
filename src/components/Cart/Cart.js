3dbd641af4cca8ff4148d39de2ede0edb5dde8f7
shilpi <shilpi8624@gmail.com>
2024-06-13 16:58:22 +0530

    :airplane: created cart modal

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import classes from "./Cart.module.css"
+import Modal from '../UI/Modal'
+
+const Cart = (props) => {
+    const cartItem = (
+        <ul className={classes["cart-items"]}>
+            {[{id:"c1", name:"Sushi", amount:2, price: 12.99}].map((item) => {
+                return (<li>{item.name}</li>)
+            })}
+        </ul>
+    )
+  return (
+    <Modal>
+        {cartItem}
+    <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.65</span>
+    </div>
+    <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+    </div>
+    </Modal>
+
+  )
+}
+
+export default Cart
